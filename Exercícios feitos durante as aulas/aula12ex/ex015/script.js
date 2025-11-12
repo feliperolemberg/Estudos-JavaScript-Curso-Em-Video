a85db91ee6c832b2856ba69282367f95bdb9d518
Felipe Rolemberg Ramos <feliperolembergrms@gmail.com>
2025-11-12 13:24:03 -0300

    Atualização no código JS

--- Exercícios feitos durante as aulas/aula12ex/ex015/script.js
+++ Exercícios feitos durante as aulas/aula12ex/ex015/script.js
@@ -10,35 +10,35 @@ function verificar() {
     var fSex = document.getElementsByName('radsex');
     var idade = ano - Number(fAno.value);
     var genero = '';
-    var img = document.createElement('img');
-    img.setAttribute('id', 'foto');
+    var imagem = document.createElement('img');
+    imagem.setAttribute('id', 'foto');
 
     if (fSex[0].checked) {
       genero = 'homem';
       if (idade >= 0 && idade < 12) {
-        img.setAttribute('src', 'imagens/homem-bebe.jpg');
+        imagem.setAttribute('src', 'imagens/homem-bebe.jpg');
       } else if (idade < 24) {
-        img.setAttribute('src', 'imagens/homem-jovem.jpg');
+        imagem.setAttribute('src', 'imagens/homem-jovem.jpg');
       } else if (idade < 50) {
-        img.setAttribute('src', 'imagens/homem-adulto.jpg');
+        imagem.setAttribute('src', 'imagens/homem-adulto.jpg');
       } else {
-        img.setAttribute('src', 'imagens/homem-idoso.png');
+        imagem.setAttribute('src', 'imagens/homem-idoso.png');
       }
     } else {
       genero = 'mulher';
       if (idade >= 0 && idade < 12) {
-        img.setAttribute('src', 'imagens/mulher-bebe.jpg');
+        imagem.setAttribute('src', 'imagens/mulher-bebe.jpg');
       } else if (idade < 24) {
-        img.setAttribute('src', 'imagens/mulher-jovem.jpg');
+        imagem.setAttribute('src', 'imagens/mulher-jovem.jpg');
       } else if (idade < 50) {
-        img.setAttribute('src', 'imagens/mulher-adulta.jpg');
+        imagem.setAttribute('src', 'imagens/mulher-adulta.jpg');
       } else {
-        img.setAttribute('src', 'imagens/mulher-idosa.png');
-      }
+        imagem.setAttribute('src', 'imagens/mulher-idosa.png');
+      } 
     }
 
     resultado.style.textAlign = 'center';
-    resultado.innerHTML = `Detectamos ${genero} com ${idade} ano(s).`;
+    resultado.innerHTML = `Detectamos um(a) ${genero} com ${idade} ano(s).`;
     resultado.appendChild(img);
   }
 
