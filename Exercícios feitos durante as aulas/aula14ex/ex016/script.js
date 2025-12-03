f25327cb46368da6d5d59c3e8c12ecb11f6b15d1
Felipe Rolemberg Ramos <feliperolembergrms@gmail.com>
2025-12-03 14:59:07 -0300

    Realização do Exercício 16

--- Exercícios feitos durante as aulas/aula14ex/ex016/script.js
+++ Exercícios feitos durante as aulas/aula14ex/ex016/script.js
@@ -1,12 +1,31 @@
-var paragrafoResposta = document.createElement('p');
-document.getElementById('caixaPrincipal').appendChild(paragrafoResposta);
-
 function listar() {
-  paragrafoResposta.innerHTML = "";
+  // paragrafoResposta.innerHTML = "";
   var numeroInicio = Number(window.document.getElementById('txtInicio').value);
   var numeroFim = Number(window.document.getElementById('txtFim').value);
   var numeroPulo = Number(window.document.getElementById('txtPulo').value);
-  for (numeroInicio; numeroInicio <= numeroFim; numeroInicio += numeroPulo) {
-    paragrafoResposta.innerHTML += `${numeroInicio}&#x1F449;`;
+  var paragrafoResposta = window.document.getElementById('resultado');
+  if (window.document.getElementById('txtInicio').value.length == 0 || window.document.getElementById('txtFim').value.length == 0 || window.document.getElementById('txtPulo').value.length == 0) {
+    paragrafoResposta.innerHTML = `Impossível contar.`;
+    window.alert(`[Erro] Faltam dados!`);
+  } else {
+    paragrafoResposta.innerHTML = `Contando...<br>`;
+    if (numeroPulo == 0 ) {
+      window.alert(`Passo inválido! Consideraremos passo 1.`);
+      numeroPulo = 1;
+    }
+    // Contagem crescente
+    if (numeroInicio < numeroFim) {
+      for (var i = numeroInicio; i <= numeroFim; i += numeroPulo) {
+        paragrafoResposta.innerHTML += `${i} &#x1F449; `;
+      }
+      paragrafoResposta.innerHTML += `&#x1F3C1;`;
+    } else if (numeroInicio == numeroFim) {
+        paragrafoResposta.innerHTML += `O início e o final são iguais! Tente novamente.`;
+    } else { // Contagem decrescente
+        for (var i = numeroInicio; i >= numeroFim; i -= numeroPulo) {
+          paragrafoResposta.innerHTML += `${i} &#x1F449; `;
+        }
+        paragrafoResposta.innerHTML += `&#x1F3C1`;
+    }
   }
 }
