0d2343f9d52bab2b9d63dc9fed2006334a240021
Felipe Rolemberg Ramos <feliperolembergrms@gmail.com>
2025-12-08 19:46:56 -0300

    Exercício 17

---
+++ Exercícios feitos durante as aulas/aula14ex/ex017/script.js
@@ -0,0 +1,17 @@
+function tabuada() {
+  var numero = window.document.getElementById('txtn').value;
+  var areaTabuada = window.document.getElementById('seltab');
+  if (numero.length == 0) {
+    window.alert('Por favor, digite um número!');
+  } else {
+    numero = Number(numero);
+    var c = 1;
+    areaTabuada = '';
+    while (c <= 10) {
+      var item = document.createElement('option');
+      item.textContent = `${numero} x ${c} = ${numero*c}`;
+      areaTabuada.appendChild(item);
+      c++;
+    }
+  }
+}
